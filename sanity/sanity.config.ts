70a2476fff8f66030d0199cd540c9247e877a734
ianbb <ianbbdai@gmail.com>
2025-03-08 17:30:13 -0800

    run prettier

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,18 +1,18 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./schemaTypes";
 
 export default defineConfig({
-  name: 'default',
-  title: 'venushacks-site-2025',
+  name: "default",
+  title: "venushacks-site-2025",
 
-  projectId: '5qebsusz',
-  dataset: 'production',
+  projectId: "5qebsusz",
+  dataset: "production",
 
   plugins: [structureTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
   },
-})
+});
