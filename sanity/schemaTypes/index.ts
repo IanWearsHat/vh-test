5ee3e987429aac66faa465c2a657556db17ec271
ianbb <ianbbdai@gmail.com>
2025-03-08 17:29:05 -0800

    Resources added to sanity

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,1 +1,5 @@
-export const schemaTypes = []
+import resource from "./resource";
+import resourceCategory from "./resourceCategory";
+import resourceCategoryOrder from "./resourceCategoryOrder";
+
+export const schemaTypes = [resource, resourceCategory, resourceCategoryOrder];
